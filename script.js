9d666f8ff2560fd49cc8f26c1da982af6a0f21fd
N-PC <muhammadnad1001100@gmail.com>
2023-09-10 21:38:14 +0500

    added responsive behaviour on profile and added cloud computing icons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    fetch("./config/config.json")
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./config/config.json")
     .then(response => response.json())
-    .then(data =>{
+    .then(data => {
       console.log(data.name)
       document.getElementById("name").innerText = data.name
-    document.getElementById("jobTitle").innerText = data.jobTitle
-    document.getElementById("about").innerText = data.about
-
+      document.getElementById("jobTitle").innerText = data.jobTitle
+      document.getElementById("about").innerText = data.about
+      document.querySelector("#calendly a").href = data.calendly
+      document.querySelector("#linkedin").href = data.linkedin
+      document.querySelector("#github").href = data.github
     })
-  })
+})
