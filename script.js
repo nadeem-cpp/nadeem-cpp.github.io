b802901f40b62b214c206860a4bdeb16d425b357
N-PC <muhammadnad1001100@gmail.com>
2024-01-19 09:26:39 +0500

    added responsiveness

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("./config/config.json")
     .then(response => response.json())
     .then(data => {
-      console.log(data.name)
+      // console.log(data.name)
       document.getElementById("name").innerText = data.name
       document.getElementById("jobTitle").innerText = data.jobTitle
       document.getElementById("about").innerText = data.about
